75ce98949677da9dc468135a849b1f6bdbf2326d
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-11-23 19:20:33 +0200

    Add login functionality for user authentication

---
+++ app/Http/Controllers/API/V1/Auth/AuthenticatedUserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginUserRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedUserController extends Controller
+{
+    public function login(LoginUserRequest $request) {
+        $credentials = $request->validated();
+            if(Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $token = $user->createToken('auth_token.' . $user->username)->plainTextToken;
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'User logged in successfully',
+                    'data' => [
+                        'user' => new UserResource($user),
+                        'token' => $token
+                    ]
+                ]);
+            }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\V1\Auth\AuthenticatedUserController;
 use App\Http\Controllers\API\V1\Auth\RegisterUserController;
 
 Route::prefix('v1')->group(function () {
     Route::post('/register', [RegisterUserController::class,'register'])->name('register');
+    Route::post('/login', [AuthenticatedUserController::class,'login'])->name('login');
 });
 
 
