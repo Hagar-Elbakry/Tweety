b85b5b3c29ea45e3178d42231f8ff2f994c84fab
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-11-23 18:29:21 +0200

    Implement user registraion feature

---
+++ app/Http/Controllers/API/V1/Auth/RegisterUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterUserController extends Controller
+{
+    public function register(RegisterUserRequest $request) {
+            $data = $request->validated();
+            $data['password'] = Hash::make($data['password']);
+            $user = User::query()->create($data);
+            $token = $user->createToken('auth_token.' . $user->username)->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User created successfully',
+                'data' => [
+                    'user' => new UserResource($user),
+                    'token' => $token,
+                ]
+            ]);
+    }
+}
