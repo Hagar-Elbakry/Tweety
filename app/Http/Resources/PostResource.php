b7167a7505c73efc873cdc7999f2b1d58a431097
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-02-11 18:39:11 +0200

    add bookmark count to PostResource for displaying total bookmarks

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
+use Maize\Markable\Models\Bookmark;
 use Maize\Markable\Models\Like;
 
 class PostResource extends JsonResource
@@ -21,6 +22,7 @@ class PostResource extends JsonResource
             'body' => $this->body ?: null,
             'image' => $this->image ? Storage::url($this->image) : null,
             'likes_count' => Like::count($this->resource),
+            'bookmark_count' => Bookmark::count($this->resource),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'user' => new UserResource($this->whenLoaded('user')),
