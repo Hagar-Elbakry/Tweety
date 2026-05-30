a897538507c097674ef0460aace6ba0e81b04357
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-30 14:46:44 +0300

    refactor: remove unused broadcast-related code from UserRegistered event

--- app/Events/UserRegistered.php
+++ app/Events/UserRegistered.php
@@ -4,7 +4,6 @@ namespace App\Events;
 
 use App\Models\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -18,17 +17,7 @@ class UserRegistered
     public function __construct(
         public User $user,
         public $otpCode
-    ) {}
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+    ) {
     }
+    
 }
