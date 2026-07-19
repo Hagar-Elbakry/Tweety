e6a7a11ce440ff9b280f93d57a268a3f42f8aed9
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-19 21:38:14 +0300

    test: add feature tests for post comments (create, retrieve, delete with proper access checks)

---
+++ tests/Feature/Post/CommentTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Models\User;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $otherUser = User::factory()->create();
+    $this->post = $otherUser->posts()->create(['body' => 'test post']);
+});
+
+it('can comment on a post', function () {
+    $response = $this->actingAs($this->user, 'sanctum')->postJson(route('posts.comments.store', $this->post),
+        ['body' => 'test comment']);
+    $response->assertStatus(200);
+    $this->assertDatabaseHas('comments', ['body' => 'test comment']);
+});
+
+it('can get comments on a post', function () {
+    $this->post->comments()->create([
+        'body' => 'test comment',
+        'user_id' => $this->user->id
+    ]);
+    $response = $this->actingAs($this->user, 'sanctum')->getJson(route('posts.comments.index', $this->post));
+    $response->assertStatus(200);
+    $response->assertJsonCount(1, 'data');
+});
+
+it('can delete a comment on a post by comment owner', function () {
+    $comment = $this->post->comments()->create([
+        'body' => 'test comment',
+        'user_id' => $this->user->id
+    ]);
+    $response = $this->actingAs($this->user, 'sanctum')->deleteJson(route('comments.destroy', [$comment]));
+    $response->assertStatus(200);
+    $this->assertDatabaseMissing('comments', ['body' => 'test comment']);
+});
+
+it('can delete a comment on a post by post owner', function () {
+    $comment = $this->post->comments()->create([
+        'body' => 'test comment',
+        'user_id' => $this->user->id
+    ]);
+    $response = $this->actingAs($this->post->user, 'sanctum')->deleteJson(route('comments.destroy',
+        [$comment]));
+    $response->assertStatus(200);
+    $this->assertDatabaseMissing('comments', ['body' => 'test comment']);
+});
+
+it('cannot delete a comment on a post by other user', function () {
+    $otherUser = User::factory()->create();
+    $comment = $this->post->comments()->create([
+        'body' => 'test comment',
+        'user_id' => $this->user->id
+    ]);
+    $response = $this->actingAs($otherUser, 'sanctum')->deleteJson(route('comments.destroy', [$comment]));
+    $response->assertStatus(403);
+});
