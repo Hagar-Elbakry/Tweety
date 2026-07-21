4d9224a986d225777384eec015820b247cdbecaf
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-07-21 14:48:29 +0300

    test: add follow and unfollow functionality tests for user interactions

---
+++ tests/Feature/Profile/FollowTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->otherUser = User::factory()->create();
+});
+
+it('can follow user', function () {
+    $response = $this->actingAs($this->user, 'sanctum')->postJson(route('follow'), [
+        'user_id' => $this->otherUser->id,
+    ]);
+    $response->assertStatus(200);
+    $response->assertJson([
+        'message' => 'Successfully followed the user.',
+    ]);
+    $this->assertDatabaseCount('follows', 1);
+});
+
+it('can unfollow user', function () {
+    $this->actingAs($this->user, 'sanctum')->postJson(route('follow'), [
+        'user_id' => $this->otherUser->id,
+    ]);
+    $response = $this->actingAs($this->user, 'sanctum')->postJson(route('follow'), [
+        'user_id' => $this->otherUser->id,
+    ]);
+    $response->assertStatus(200);
+    $response->assertJson([
+        'message' => 'Successfully unfollowed the user.',
+    ]);
+    $this->assertDatabaseCount('follows', 0);
+});
+it('can not follow myself', function () {
+    $response = $this->actingAs($this->user, 'sanctum')->postJson(route('follow'), [
+        'user_id' => $this->user->id,
+    ]);
+    $response->assertStatus(422);
+});
