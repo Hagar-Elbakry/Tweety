2166c72373bda4461347e46ae4d5fca3c049424d
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-01 19:18:09 +0300

    fix: change order of replies to ascending by creation date

--- app/Actions/LoadMoreReplyAction.php
+++ app/Actions/LoadMoreReplyAction.php
@@ -11,7 +11,7 @@ final class LoadMoreReplyAction
     {
         return Comment::where('parent_id', $comment->id)->with('user:id,name,username,avatar')
             ->withCount('replies')
-            ->orderBy('created_at', 'desc')
+            ->orderBy('created_at', 'asc')
             ->cursorPaginate(3);
     }
 }
