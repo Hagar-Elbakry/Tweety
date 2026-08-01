bcea4f0dc7f031742d831646e822eb2ef000ed9b
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-08-01 19:57:34 +0300

    feat: add tests for retrieving replies of a reply and pagination of replies

--- tests/Feature/Post/CommentTest.php
+++ tests/Feature/Post/CommentTest.php
@@ -29,6 +29,90 @@ it('can get comments on a post', function () {
     $response->assertJsonCount(1, 'data');
 });
 
+it('can get replies of a reply', function () {
+    $comment = $this->post->comments()->create([
+        'body' => 'test comment',
+        'user_id' => $this->user->id,
+    ]);
+    $reply = $comment->replies()->create([
+        'body' => 'test reply',
+        'post_id' => $this->post->id,
+        'user_id' => $this->user->id,
+        'parent_id' => $comment->id,
+    ]);
+    $replyOfReply = $reply->replies()->create([
+        'body' => 'test reply of reply',
+        'post_id' => $this->post->id,
+        'user_id' => $this->user->id,
+        'parent_id' => $reply->id,
+    ]);
+    $response = $this->actingAs($this->user, 'sanctum')->getJson(route('comments.replies.index', $reply));
+    $response->assertStatus(200);
+    $response->assertJson([
+        'data' => [
+            'comments' => [
+                [
+                    'id' => $replyOfReply->id,
+                    'body' => 'test reply of reply',
+                    'user' => [
+                        'name' => $this->user->name,
+                        'username' => $this->user->username,
+                        'avatar' => $this->user->avatar,
+                    ],
+                    'has_more_replies' => false,
+                ],
+            ]
+        ]
+    ]);
+});
+
+it('returns next page of replies using cursor', function () {
+    $comment = $this->post->comments()->create([
+        'body' => 'test comment',
+        'user_id' => $this->user->id,
+    ]);
+    $reply = $comment->replies()->create([
+        'body' => 'test reply',
+        'post_id' => $this->post->id,
+        'user_id' => $this->user->id,
+        'parent_id' => $comment->id,
+    ]);
+    $reply->replies()->createMany([
+        [
+            'body' => 'test reply of reply1',
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
+            'parent_id' => $reply->id
+        ],
+        [
+            'body' => 'test reply of reply 2',
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
+            'parent_id' => $reply->id
+        ],
+        [
+            'body' => 'test reply of reply 3',
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
+            'parent_id' => $reply->id
+        ],
+        [
+            'body' => 'test reply of reply 4',
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
+            'parent_id' => $reply->id
+        ],
+    ]);
+    $response = $this->actingAs($this->user, 'sanctum')->getJson(route('comments.replies.index', $reply));
+    $response->assertStatus(200);
+    $response->assertJsonCount(3, 'data.comments');
+    $nextPageUrl = $response->json('data.next_page_url');
+    $this->actingAs($this->user, 'sanctum')
+        ->getJson($nextPageUrl)
+        ->assertStatus(200)
+        ->assertJsonCount(1, 'data.comments');
+});
+
 it('can delete a comment on a post by comment owner', function () {
     $comment = $this->post->comments()->create([
         'body' => 'test comment',
