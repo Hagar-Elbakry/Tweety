e88063e14d052d6b8026e404a823d821de904fc0
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-09-14 00:18:39 +0300

    feat: move TypingController to API V1 namespace

--- app/Http/Controllers/API/V1/TypingController.php
+++ app/Http/Controllers/API/V1/TypingController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\V1;
 
 use App\Events\UserTyping;
 use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\TypingRequest;
 use App\Models\Conversation;
 
